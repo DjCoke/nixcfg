e5c43ec10798251bd5bc3cd623cab97e4a4d5449
Erwin van de Glind <erwin.vd.glind@me.com>
2024-11-02 21:34:51 +0100

    Removed repl-flakes from home/common/default.nix

--- home/common/default.nix
+++ home/common/default.nix
@@ -29,7 +29,7 @@
   nix = {
     package = lib.mkDefault pkgs.nix;
     settings = {
-      experimental-features = [ "nix-command" "flakes" "repl-flake" ];
+      experimental-features = [ "nix-command" "flakes" ]; #removed repl-flakes
       warn-dirty = false;
     };
   };
